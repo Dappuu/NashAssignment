603f79e9b3abe14e02d887419bab93888b3b3780
Dat Bui <bvdat1110@gmail.com>
2024-05-21 17:21:42 +0700

    - NavBar component for Category and SubCategory - Home View - Category View and CategoryDetail

---
+++ CustomerBackEnd/ViewComponents/NavBarViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using CustomerBackEnd.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ViewModels.Category;
+
+namespace CustomerBackEnd.ViewComponents
+{
+	public class NavBarViewComponent : ViewComponent
+	{
+		private readonly IHttpClientFactory _clientFactory;
+
+		public NavBarViewComponent(IHttpClientFactory clientFactory)
+		{
+			_clientFactory = clientFactory;
+		}
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			HttpClient client = _clientFactory.CreateClient(name: "BackEndApi");
+			string url = $"api/category";
+			HttpResponseMessage response = await client.GetAsync(url);
+			if (!response.IsSuccessStatusCode)
+			{
+				return View(new List<CategoryDto>());
+			}
+			var content = await response.Content.ReadAsStringAsync();
+			List<CategoryDto>? categoriesDto = JsonConvert.DeserializeObject<List<CategoryDto>>(content);
+			if (categoriesDto == null)
+			{
+				return View(new List<CategoryDto>());
+			}
+			return View(categoriesDto);
+		}
+	}
+}
